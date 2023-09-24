22b7a1949353535b68e5a9fe0a9c911369cbbd4a
Rustam Ibragimov <rustamibragimov67@yandex.ru>
2023-09-24 07:03:30 +0500

    Уточнено ПЗ №1

--- practical_tasks/PT1/task2.py
+++ practical_tasks/PT1/task2.py
@@ -1,15 +1,17 @@
 # Задача №2
 # Написать точно такую же процедуру, но в декларативном стиле
 
-numbers = [3, -5, 2, 1, 10, -8, 6, 77, -15]
-print(f"Before list: {numbers}")
-
 # Вариант 1
+numbers1 = [3, -5, 2, 1, 10, -8, 6, 77, -15]
+print(f"Before list1: {numbers1}")
+
 def sort_list_declarative(numbers):
     numbers.sort(reverse=True)
     return numbers
 
-print(f"After list Var 1: {sort_list_declarative(numbers)}")
+print(f"After list1: {sort_list_declarative(numbers1)}")
 
 # Вариант 2
-print(f"After list Var 2: {sorted(numbers, reverse=True)}")
+numbers2 = [3, -5, 2, 1, 10, -8, 6, 77, -15]
+print(f"Before list2: {numbers2}")
+print(f"After list2: {sorted(numbers2, reverse=True)}")
